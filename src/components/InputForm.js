280d8bc50f890bd937998a8f30ec16756a37484c
Hyunjae Kim <peterhyunjae@naver.com>
2022-03-21 20:15:45 +0900

    💄 [style]: form layout finished

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 const InputForm = () => {
   return (
     <Container>
-      <h1>hi</h1>
+      <Form>
+        <InputField placeholder="할 일을 입력하세요" />
+        <Button>확인</Button>
+      </Form>
     </Container>
   );
 };
@@ -13,5 +16,34 @@ const Container = styled.div`
   flex-direction: row;
   height: 50px;
   align-self: end;
+
+  justify-content: center;
+  align-items: center;
 `;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: 'row';
+`;
+const InputField = styled.input`
+  height: 30px;
+  width: 240px;
+  text-align: center;
+
+  border-radius: 20px;
+  outline: none;
+
+  margin-right: 15px;
+`;
+const Button = styled.button`
+  border: none;
+  background: red;
+  border-radius: 4px;
+
+  color: #ffffff;
+  text-align: center;
+  text-decoration: none;
+  font-size: 14px;
+`;
+
 export default InputForm;
