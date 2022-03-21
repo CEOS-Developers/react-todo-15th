34267541aa174c77fcb061e91db58cc02d1d20b9
juhyojeong <juhyojeong58@gmail.com>
2022-03-21 12:13:45 +0900

    feat: add todo to done toggle

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { useState } from "react";
 function App() {
   const [newTodoText, setNewTodoText] = useState("");
   const [todoList, setTodoList] = useState([]);
+  const [doneList, setDoneList] = useState([]);
 
   const _changeText = (event) => {
     setNewTodoText(event.target.value);
   };
 
+  // todo item 추가
   const _addTodoItem = (e) => {
     e.preventDefault();
 
@@ -22,6 +24,15 @@ function App() {
     // list에 todo item 추가
     setTodoList([...todoList, todoObj]);
     setNewTodoText("");
+  };
+
+  // todo -> done
+  const _toggleToDone = (e) => {
+    const parentNode = e.target.parentNode;
+    const idx = parentNode.id;
+
+    setDoneList([...doneList, todoList[idx]]);
+    todoList.splice(idx, 1);
   };
 
   return (
@@ -50,9 +61,28 @@ function App() {
             TO DO (<span>{todoList.length}</span>)
           </h3>
           <ul className="todo-list">
-            {todoList.map((todo) => (
-              <li id={todo.id} className="todo-list-item">
-                <span className="todo-item-text">{todo.text}</span>
+            {todoList.map((todo, idx) => (
+              <li key={todo.id} id={idx} className="todo-list-item">
+                <span className="todo-item-text" onDoubleClick={_toggleToDone}>
+                  {todo.text}
+                </span>
+                <img
+                  className="todo-delete-btn"
+                  src="bin.png"
+                  alt="delete-btn"
+                />
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section className="todo-contents">
+          <h3>
+            DONE (<span>{doneList.length}</span>)
+          </h3>
+          <ul className="todo-list">
+            {doneList.map((done, idx) => (
+              <li key={done.id} id={idx} className="todo-list-item">
+                <span className="done-item-text">{done.text}</span>
                 <img
                   className="todo-delete-btn"
                   src="bin.png"
