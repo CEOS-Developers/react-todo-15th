97c188590d411390a6a887879896d392ef5b3688
Daeheon Jeong <daeheon_macbook@Daeheons-MacBook-Air.local>
2022-03-23 16:50:17 +0900

    ADD: 아이템 리스트의 CSS 고정 높이 및 너비, 스크롤 가능 속성 추가

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-const ListItem = () => {
-  return <div>ListItem</div>;
+const ListItem = (props) => {
+  return (
+    <div class="item" id="">
+      <div class="itemScrollArea">
+        <button class="itemFinish" type="button">
+          {props.isDone ? (
+            <p class="itemTitle">
+              <del>title</del>
+            </p>
+          ) : (
+            <p class="itemTitle">loremsdgasdgjuiqhioweuhqiwuhiquwhituhquiweh</p>
+          )}
+        </button>
+      </div>
+      <button class="itemDelete" type="button">
+        <p>
+          {/* <i class="fa fa-solid fa-trash"></i> */}
+          delete
+        </p>
+      </button>
+    </div>
+  );
 };
 
 export default ListItem;
