d806f2b53506f3637a5aed347192759efbe59163
24siefil <24siefil@gmail.com>
2022-03-24 19:03:05 +0900

    FEAT: Add input form

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Screen.css';
+import { BsPlusCircle } from 'react-icons/bs';
 
 const Screen = ({ children, todoLength }) => {
   return (
     <div className="Screen">
-      <header className="title">Todo List ( {todoLength} / 0 )</header>
-      <section>{children}</section>
+      <header className="Title">Todo List ( {todoLength} / 0 )</header>
+      <section className="TodoListContainer">{children}</section>
+      <form className="FormContainer">
+        <input
+          autoFocus
+          className="Input"
+          placeholder=" Please enter here..."
+        />
+        <button className="Button">
+          <BsPlusCircle />
+        </button>
+      </form>
     </div>
   );
 };
 
 export default Screen;
+
+/*
+React className naming convention
+https://stackoverflow.com/questions/57221878/react-classname-naming-convention
+*/
