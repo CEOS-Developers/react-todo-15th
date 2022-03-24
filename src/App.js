006b1cdbecf24c2501cc73ce576280d4ada4b318
24siefil <24siefil@gmail.com>
2022-03-24 12:38:29 +0900

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>15기 화이팅~</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
+
+// importing React 와 JSX 사용의 상관관계
+// https://hwani.dev/import-react/
