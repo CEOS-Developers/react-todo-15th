1fd3269144937d64f4ff4bd6df0fce832b1b863d
probloom-solbin-de <101960628+probloom-solbin-de@users.noreply.github.com>
2022-03-25 11:38:36 +0900

    Feat : 엘리먼트 렌더 추가
    엘리먼트 렌더 1차 추가 및 환경설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from 'react';
+import logo192 from './logo192.png';
+import logo512 from './logo512.png';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>15기 화이팅~</h1>
+    <div className = "App">
+      <div className = "Title">
+        <h1>투두</h1>
+        <img src = {logo}/>
+      </div>
+      
     </div>
-  );
-}
+    /*
+    const element = <form className = "input-box">;
+    const element = <div><input
+      type="text"
+      id="todo-input"
+      placeholder="오늘 할 일을 입력하세요."/>
+    <input type="button" value="+" onclick="addNewTodo()" /div>
+    </form>
+    const element = <div>
+    <section className="todo-list-box">
+      <h3 id="todo-list-title">오늘 할 일은 다음과 같아요.</h3>
+      <ul id="todo-list"></ul>
+    </section>
+    </div>;
+    */
+  )
+};
 
 export default App;
