a9ae2e3ebf42caa1f55b78ff3a4762e51af814ce
YoommY <seeun0792@gmail.com>
2022-03-25 15:30:49 +0900

    Clock.js

---
+++ src/clock.js
@@ -0,0 +1,15 @@
+function Clock() {
+    const date = new Date();
+   
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    
+   
+    
+   return <StyledButton>{`${hours}:${minutes}:${seconds}`}</StyledButton>;
+     
+}
+ 
+setInterval(Clock, 1000);
+export default Clock;
