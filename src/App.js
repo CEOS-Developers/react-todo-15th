ae643e27dbff3a050f1213dcfcb5d8b6c7c30caf
한규진 <david0218@naver.com>
2022-03-30 14:38:26 +0900

    refactor : app.js에서 props 내려주던거 삭제 / tsconfig.json

--- src/App.js
+++ src/App.js
@@ -8,24 +8,19 @@ import InputTodo from './components/InputTodo';
 import TodoItemList from './components/TodoItemList';
 
 function App() {
-    // list 상태 변화할 때마다 로컬스토리지 동기화
-    const { list, setList } = useContext(TodoContext);
-    useEffect(() => {
-        console.log(list);
-    }, [list]);
     return (
         <>
             <GlobalStyle />
             <Main>
                 <Section>
                     <h1>투두 리스트</h1>
-                    <InputTodo list={list} setList={setList} />
+                    <InputTodo />
                 </Section>
                 <Section>
-                    <TodoItemList done={false} list={list} setList={setList}></TodoItemList>
+                    <TodoItemList done={false}></TodoItemList>
                 </Section>
                 <Section>
-                    <TodoItemList done={true} list={list} setList={setList}></TodoItemList>
+                    <TodoItemList done={true}></TodoItemList>
                 </Section>
                 <ToastComponent />
             </Main>
