023c79f6490317b9e05c9c741e945ff6e7b6d826
YoommY <seeun0792@gmail.com>
2022-04-01 06:54:26 +0900

    chore : usetoDo에 대한 코멘트 추가

--- src/hooks/useToDo.tsx
+++ src/hooks/useToDo.tsx
@@ -1,23 +1,22 @@
 import {IProps} from "../type/type";
 
 
-
 const useToDo=({type, setDoneToDoList, list , setToDoList} : IProps) => {
   
   
   const setToDo = (item : string, id :number) => {
                            if(type === "Todo") 
-                           setToDoList(list.filter((item, index) => id !== index));
+                           setToDoList(list.filter((item, index) => id !== index)); //todolist remove
                           else
-                           setDoneToDoList(list.filter((item, index) => id !== index));
+                           setDoneToDoList(list.filter((item, index) => id !== index)); //donelist remove
                           
                           }
 
   const setDoneToDo = (item : string, id :number) => {
                              if(type === "Todo") 
-                               setDoneToDoList((ItemList: Array<string>) => [item, ...ItemList]);
+                               setDoneToDoList((ItemList: Array<string>) => [item, ...ItemList]); //todolist 추가
                              else
-                                setToDoList((ItemList: Array<string>) => [item, ...ItemList]);
+                                setToDoList((ItemList: Array<string>) => [item, ...ItemList]); //donelist 추가
   }
   const deleteButton = (item : string, id :number) => {
      
@@ -27,7 +26,7 @@ const useToDo=({type, setDoneToDoList, list , setToDoList} : IProps) => {
         
       const moveButton = (item : string, id :number) =>{
         
-       setDoneToDo(item,id);
+       setDoneToDo(item,id); //토글이 일어나면 삭제하고 다른 리스트에 추가하기
        setToDo(item,id);
       
       };
