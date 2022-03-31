9e146653f28998344856084d2ee83fe285072c72
24siefil <24siefil@gmail.com>
2022-03-31 10:16:16 +0900

    REFACTOR: TodoList에 styled-components 적용

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,15 @@
 import TodoItem from './TodoItem';
-import './TodoList.css';
+import styled from 'styled-components';
+// import './TodoList.css';
 
 const TodoList = ({ todos, doneCnt, todoCnt, onIsDoneToggle, onRemove }) => {
   const todoItems = todos.filter((todo) => todo.isDone === false);
   const doneItems = todos.filter((todo) => todo.isDone === true);
   return (
     <div>
-      <header className="TodoTitle">Todo ({todoCnt})</header>
-      <section className="TodoList">
+      {/* <header className="TodoTitle">Todo ({todoCnt})</header> */}
+      <TodoHeader>Todo ({todoCnt})</TodoHeader>
+      {/* <section className="TodoList">
         {todoItems.map((todo) => (
           <TodoItem
             todo={todo}
@@ -16,9 +18,21 @@ const TodoList = ({ todos, doneCnt, todoCnt, onIsDoneToggle, onRemove }) => {
             onRemove={onRemove}
           />
         ))}
-      </section>
-      <header className="DoneTitle">Done ({doneCnt})</header>
-      <section className="DoneList">
+      </section> */}
+      <TodoSection>
+        {todoItems.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onIsDoneToggle={onIsDoneToggle}
+            onRemove={onRemove}
+          />
+        ))}
+      </TodoSection>
+
+      {/* <header className="DoneTitle">Done ({doneCnt})</header> */}
+      <DoneHeader>Done ({doneCnt})</DoneHeader>
+      {/* <section className="DoneList">
         {doneItems.map((todo) => (
           <TodoItem
             todo={todo}
@@ -27,9 +41,43 @@ const TodoList = ({ todos, doneCnt, todoCnt, onIsDoneToggle, onRemove }) => {
             onRemove={onRemove}
           />
         ))}
-      </section>
+      </section> */}
+      <DoneSection>
+        {doneItems.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onIsDoneToggle={onIsDoneToggle}
+            onRemove={onRemove}
+          />
+        ))}
+      </DoneSection>
     </div>
   );
 };
+
+const TodoHeader = styled.header`
+  margin: 6% 0% 3% 3%;
+
+  font-size: 1.4rem;
+  font-weight: bold;
+`;
+
+const TodoSection = styled.section`
+  height: 260px;
+  overflow: auto;
+`;
+
+const DoneHeader = styled.header`
+  margin: 4% 0% 3% 3%;
+
+  font-size: 1.4rem;
+  font-weight: bold;
+`;
+
+const DoneSection = styled.section`
+  height: 170px;
+  overflow: auto;
+`;
 
 export default TodoList;
