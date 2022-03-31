dc76fa85c4e1019a4c3fffac864c3e10511756f8
YoommY <seeun0792@gmail.com>
2022-04-01 06:56:26 +0900

    chore : type에 대한 코멘트 추가

--- src/type/type.tsx
+++ src/type/type.tsx
@@ -1,6 +1,8 @@
 import { Dispatch, SetStateAction} from "react";
 
-export interface IProps{
+
+//todo,done 파일에서 usetoDo를 불러와야하므로 list,Itemlist가 속성은 같지만 이름을 달리해야 했음
+export interface IProps{ 
 
   type : string;
   setDoneToDoList:  Dispatch<SetStateAction<string[]>>;
@@ -9,7 +11,7 @@ export interface IProps{
 
 }
 
-export interface ItemList{
+export interface ItemList{ 
 
   type : string;
   setDoneToDoList: Dispatch<SetStateAction<Array<string>>>;
