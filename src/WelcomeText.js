d05113522f2a2d46779c12fda10cd0c62bcb0bb5
probloom-solbin-de <101960628+probloom-solbin-de@users.noreply.github.com>
2022-04-01 16:15:50 +0900

    Style : style component 적용
    타이틀 스타일 컴포넌트로 변경

--- src/WelcomeText.js
+++ src/WelcomeText.js
@@ -1,17 +1,48 @@
 import React from "react";
+import styled from 'styled-components';
+
+const WelcomBox = styled.div`
+    margin: 20px 20px 20px 20px;
+`;
+
+const WelcomTitle = styled.div`
+    margin: 20px 20px 20px 20px;
+    width: 100vw;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    font-size: 1.2rem;
+    font-weight: Bold;
+`; 
+
+const WelcomText = styled.div`
+    margin: 20px 20px 20px 20px;
+    width: 100%;
+    display: block;
+`; 
+
+const TitleInfo = styled.span`
+    display: span;
+`;
+
+const TitleName = styled.span`
+    padding-left: 10px;
+    display: span;
+    color: blue;
+`;
 
 const WelcomeText = () => {
   return (
-    <div id="WelcomBox" className="Box">
-      <div id="WelcomTitle">
-        <span id="TitleInfo">집중해서 마치는 오늘 할 일</span>
+    <WelcomBox>
+      <WelcomTitle>
+        <TitleInfo>집중해서 마치는 오늘 할 일</TitleInfo>
         <span id="TitleName">기어업</span>
-      </div>
-      <div id="WelcomText">
+      </WelcomTitle>
+      <WelcomText>
         기어업은 스스로 집중해서 일을 끝내고싶은 사람들을 위해 여기 있어요.
         오늘도 함께 할 일을 시작해볼까요?
-      </div>
-    </div>
+      </WelcomText>
+    </WelcomBox>
   );
 };
 
