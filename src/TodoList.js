0426098c3d5dff4f58b457072bbb9d8fd8401ed1
probloom-solbin-de <101960628+probloom-solbin-de@users.noreply.github.com>
2022-04-01 14:06:50 +0900

    Refactor : 코드 정리

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,26 @@
 import React from "react";
-import "./App.css";
 import TodoItem from "./TodoItem.js";
 
-const TodoList = ({ todos, onRemove, onToggle }) => {
+const TodoList = ({ todos, onToggle, onDelete }) => {
   return (
-    <div>
+    <div id="ListBox" className="Box">
       <h3 id="todo-list-title">오늘 할 일은 다음과 같아요.</h3>
       <ul className="todo-list-box">
-        <TodoItem todos={todos} onRemove={onRemove} onToggle={onToggle} />
+        <TodoItem
+          todos={todos}
+          onToggle={onToggle}
+          onDelete={onDelete}
+          keyDone= {false}
+        />
+      </ul>
+      <h3 id="done-list-title">벌써 다음과 같은 일들을 해냈어요.</h3>
+      <ul className="todo-list-box">
+        <TodoItem
+          todos={todos}
+          onToggle={onToggle}
+          onDelete={onDelete}
+          keyDone= {true}
+        />
       </ul>
     </div>
   );
